02fb0a07756f30d82db965c58137fc556771216f
RJ Dellecese <rjdellecese@gmail.com>
2016-01-09 17:28:01 +0000

    added Stripe (test) payment functionality ; distinguished between premium and free courses

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,23 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_enrolled_in_course
+	
 	def show
 	end
 
 	private
 
+	def require_enrolled_in_course
+		current_course = current_lesson.section.course
+		unless current_user.enrolled_in?(current_course)
+			flash[:alert] = "You must be enrolled in this course to view the lessons"
+			redirect_to course_path(current_course)
+		end
+	end
+
 	helper_method :current_lesson
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
 	end
+
 end
